a63272a982f0b05b903841138572204da64b2ab4
taif-k <taifk817@gmail.com>
2025-02-17 13:58:26 +0530

    delete old files

--- taif/Loop/loopTestQ2.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-void main()
-{
-    double k = 0; // 0.0
-    for (k = 0.0; k < 3.0; k++)
-        ;
-    printf("%lf", k);
-}
--- taif/Loop/loopTestQ5.c
+++
@@ -1,5 +0,0 @@
-// What breaks an infinite loop?
-  // A. Break
-  // B. exit(0)
-  // C. abort()
-  // D. Terminate
