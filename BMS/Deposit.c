bd0afe0a2d44367ef523f25951b1e4d96a16ce6b
taif-k <taifk817@gmail.com>
2025-02-18 16:22:05 +0530

    Deposit function

---
+++ BMS/Deposit.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int openingbalance;
+char accountnumber[12];
+int depositamount = 0;
+
+void Deposit();
+
+int main()
+{
+    Deposit();
+    return 0;
+}
+
+void Deposit()
+{
+    while (1)
+    {
+        printf("\nEnter deposit amount ( Minimum-100 ): ");
+        scanf("%d", &depositamount);
+        if (depositamount >= 100)
+        {
+            openingbalance = openingbalance + depositamount;
+            break;
+        }
+    }
+    printf("\nAccount Number: %s", accountnumber);
+    printf("\nTotal Balance: %d", openingbalance);
+}
