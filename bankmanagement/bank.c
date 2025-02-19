efa7e7df569786f37fea427634f892331f4497cf
Indixpert-sumit <sy0050854@gmail.com>
2025-02-19 13:19:21 +0530

    menu

---
+++ bankmanagement/bank.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int main()
+{
+
+    int num;
+    int press=0;
+    char name;
+             printf("\t! welcome to world's bank !");
+                 printf("\n! Press 1 For Create Account !");
+              printf("\n! Press 2 For deposit !");
+                     printf("\n! press 3 for withdraw Amount   !");
+                     printf("\n! press 4 for Display Balance   !");
+                     printf("\n! press 5 for check balance  !");
+                     printf("\n! press 0 for Exit  !");
+
+
+    
+         printf("\n! please press  any option !");
+     scanf("%d",&num);
+
+
+
+    
+
+return 0;
+}
