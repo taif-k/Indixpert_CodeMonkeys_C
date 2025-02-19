af94057fbf7920771da87a55043805eec066035f
taif-k <taifk817@gmail.com>
2025-02-19 08:09:28 +0530

    Update function

--- BMS/DisplayAccountDetails.c
+++ BMS/DisplayAccountDetails.c
@@ -1,19 +1,13 @@
 #include <stdio.h>
 
-char holdername[10];
-int balance;
-char accountnumber[12];
-
 void DisplayAccountDetails();
 
 int main()
 {
-    DisplayAccountDetails();
-    return 0;
-}
+    char holdername[10];
+    int balance;
+    char accountnumber[12];
 
-void DisplayAccountDetails()
-{
     printf("Enter 11 digits Account number ");
     scanf(" %s", &accountnumber);
     printf("Enter Account Holder Name: ");
@@ -21,7 +15,15 @@ void DisplayAccountDetails()
     printf("Enter Account Balance: ");
     scanf(" %d", &balance);
 
-    printf("\nAccount Number: %s", accountnumber);
-    printf("\nAccount Holder Name: %s", holdername);
-    printf("\nBalance: %d", balance);
+    DisplayAccountDetails(holdername,balance,accountnumber);
+
+    return 0;
+}
+
+void DisplayAccountDetails(char hname[10],int bal,char anumb[12])
+{
+
+    printf("\nAccount Number: %s", anumb);
+    printf("\nAccount Holder Name: %s", hname);
+    printf("\nBalance: %d", bal);
 }
