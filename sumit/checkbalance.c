cb03b4f57372c4c951150bbe7793a3d249999392
Indixpert-sumit <sy0050854@gmail.com>
2025-02-20 22:37:20 +0530

    balance

--- sumit/checkbalance.c
+++ sumit/checkbalance.c
@@ -2,17 +2,24 @@
 
 // function declaration(parameter1, parameter2)
 
+
 int main()
 {
+  int balance;
+  int account;
 // balance variable
 // account varibale
+printf("%d",balance);
+printf("%d",account);
 
 // input from user for account(scanf)
-// input from user for balance(scanf)
-
+// input from user for balance(scanf) 
+scanf("%d",&balance);
+scanf("%d",&account);
 
 // calling of function(Argument1, Argument 2)
-  return 0;
+   return 0;
 }
 
 // void function(parameter1, parameter2){}
+void balance(balance , acoount);
