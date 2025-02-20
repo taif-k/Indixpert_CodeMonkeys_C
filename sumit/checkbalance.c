3f2cde5bcb3401e2d59660f10417db36a595db1d
taif-k <taifk817@gmail.com>
2025-02-20 21:59:18 +0530

    Add user defined functions

---
+++ sumit/checkbalance.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+// function declaration(parameter1, parameter2)
+
+int main()
+{
+// balance variable
+// account varibale
+
+// input from user for account(scanf)
+// input from user for balance(scanf)
+
+
+// calling of function(Argument1, Argument 2)
+  return 0;
+}
+
+// void function(parameter1, parameter2){}
