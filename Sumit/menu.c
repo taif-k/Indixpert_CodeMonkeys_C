5b2fee15bd63bad2861d9c16b703a62a5ae9b8ab
taif-k <taifk817@gmail.com>
2025-02-21 22:12:06 +0530

    Add menu file

---
+++ Sumit/menu.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+void menu();
+
+int main()
+{
+
+    menu();
+    return 0;
+}
+
+void menu()
+{
+    printf("\n1- Create Account");
+    printf("\n2- Deposit");
+    printf("\n3- Withdraw");
+    printf("\n4- Display Details");
+    printf("\n5- Check Balance");
+    printf("\n0- Exit");
+
+}
