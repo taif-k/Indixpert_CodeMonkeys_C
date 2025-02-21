082553c890efb0a9675157efc4f275790b62ffa9
Indixpert-sumit <sy0050854@gmail.com>
2025-02-21 10:01:07 +0530

    menu comments

--- sumit/menu.c
+++ sumit/menu.c
@@ -1,13 +1,14 @@
 #include<stdio.h>
+ 
 
 int main()
 {
-    printf(" Press 1 For Create Account ");
-    printf("\n Press 2 For Deposit Amount ");
-    printf("\n Press 3 For Withdraw Amount ");
-    printf("\n Press 4 For Display Balance ");
-    printf("\n Press 5 For Check Balance ");
-    printf("\n Press 0 For Exit !");
+    printf(" Press 1 For Create Account "); //Create Account
+    printf("\n Press 2 For Deposit Amount "); //Deposit
+    printf("\n Press 3 For Withdraw Amount "); //Withdraw
+    printf("\n Press 4 For Display Balance "); //Diplay Balance
+    printf("\n Press 5 For Check Balance "); // Check Balance
+    printf("\n Press 0 For Exit !");         // Exit
 
 
     return 0;
