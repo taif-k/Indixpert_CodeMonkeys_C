d36a926b054801a9065cfdeccf115873ef178864
Indixpert-sumit <sy0050854@gmail.com>
2025-02-22 11:29:31 +0530

    add checkbalance and menu

---
+++ sumit/menu.c
@@ -0,0 +1,17 @@
+int main() // System Defined Function
+{
+  
+    menu();
+ 
+    return 0;
+}
+
+void menu()
+{
+
+    printf("\n1- Create Account ");
+    printf("\n2- Enter 2 to Deposit ");
+    printf("\n3- Enter 3 to Withdraw ");
+    printf("\n4- Enter 4 to Display Balance ");
+    printf("\n5- Enter 5 to Check Balance ");
+}
