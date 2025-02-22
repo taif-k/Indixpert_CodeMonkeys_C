97ff5b1bebb7e292b352dc3ee0b91968623a104c
taif-k <taifk817@gmail.com>
2025-02-22 14:36:30 +0530

    Add header file

--- sumit/menu.c
+++ sumit/menu.c
@@ -1,17 +1,19 @@
+#include<stdio.h>
+
+void menu();
+
 int main() // System Defined Function
 {
-  
     menu();
- 
     return 0;
 }
 
 void menu()
 {
-
     printf("\n1- Create Account ");
-    printf("\n2- Enter 2 to Deposit ");
-    printf("\n3- Enter 3 to Withdraw ");
-    printf("\n4- Enter 4 to Display Balance ");
-    printf("\n5- Enter 5 to Check Balance ");
+    printf("\n2- Deposit ");
+    printf("\n3- Withdraw ");
+    printf("\n4- Display Details ");
+    printf("\n5- Check Balance ");
+    printf("\n0- Exit");
 }
